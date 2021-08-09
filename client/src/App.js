fa153175d5ed5e91d58bb02facd1c2ead7b34516
Bruno Ferreira <srbrunoferreira@outlook.com>
2021-08-08 22:11:20 -0300

    fix: material grid component troubles

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles'
 import Grid from '@material-ui/core/Grid'
+import { Box } from '@material-ui/core'
 
 import './App.css'
 import ItemsContainer from './components/ItemsContainer'
+import Text from './components/Text'
 
 const useStyles = makeStyles((theme) => ({
   container: {
@@ -12,11 +14,23 @@ const useStyles = makeStyles((theme) => ({
     padding: '15px'
   },
   subContainer: {
-    justifyContent: 'center',
-    alignItems: 'center'
+    textAlign: 'center'
   },
   white: {
     color: 'white'
+  },
+  contentWrapper: {
+    width: '390px',
+    height: '530px',
+    backgroundColor: 'rgba(255, 255, 255, 0.92)',
+    borderRadius: '3px',
+    padding: '10px',
+    textAlign: 'left'
+  },
+  header: {
+    width: '100%',
+    backgroundColor: '#EFEFEF',
+    padding: '15px'
   }
 }))
 
@@ -24,13 +38,30 @@ function App () {
   const classes = useStyles()
 
   return (
-    <Grid container className={classes.container}>
-      <Grid item className={classes.subContainer} container xs>
+    <Grid
+      className={classes.container}
+      container
+      justifyContent="center"
+      alignItems="center"
+    >
+
+      <Grid className={classes.subContainer} item xs>
         <img src="/logo.svg" width="190px" />
       </Grid>
-      <Grid item className={classes.subContainer} container xs>
-        <ItemsContainer />
+
+      <Grid className={classes.subContainer} item xs>
+
+        <Box className={classes.contentWrapper}>
+          <Box className={classes.header}>
+            <Text text="Olá, " size="lg" />
+            <Text text="Bruno" bold size="lg" />
+          </Box>
+
+          <ItemsContainer />
+        </Box>
+
       </Grid>
+
     </Grid>
   )
 }
--- client/src/components/ItemsContainer.js
+++ client/src/components/ItemsContainer.js
@@ -2,17 +2,8 @@ import React from 'react'
 import { Box } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
 
-import Text from './Text'
-
 const useStyles = makeStyles((theme) => ({
-  itemsContainer: {
-    width: '390px',
-    height: '530px',
-    backgroundColor: 'rgba(255, 255, 255, 0.92)',
-    borderRadius: '3px',
-    padding: '10px'
-  },
-  header: {
+  container: {
     width: '100%',
     backgroundColor: '#EFEFEF',
     padding: '15px'
@@ -20,14 +11,11 @@ const useStyles = makeStyles((theme) => ({
 }))
 
 const ItemsContainer = () => {
-  const classes = useStyles()
+  const style = useStyles()
 
   return (
-    <Box className={classes.itemsContainer}>
-      <Box className={classes.header}>
-        <Text text="Olá, " size="lg" />
-        <Text text="Bruno" bold size="lg" />
-      </Box>
+    <Box className={style.contaienr}>
+      <p>lorem</p>
     </Box>
   )
 }
