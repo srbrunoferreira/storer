20078ae3418a53906ad7b94a6652a51b8b9e0947
Bruno Ferreira <srbrunoferreira@outlook.com>
2021-08-12 18:56:55 -0300

    fix: wrong urls

--- api/src/routes/task_route.js
+++ api/src/routes/task_route.js
@@ -6,7 +6,7 @@ import { getTasks, getTask, createTask } from '../controllers/task_controller.js
 const router = Router()
 
 router.get('/', getTasks) // Gets all tasks.
-router.get('/tasks/:id', getTask)
+router.get('/:id', getTask) // Gets a task.
 router.post('/', createTask) // Creates a task.
 
 export default router
