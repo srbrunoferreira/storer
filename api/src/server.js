a44565b6dc903776ecb381b85a9def470ff00d0d
Bruno Ferreira <srbrunoferreira@outlook.com>
2021-08-17 08:10:15 -0300

    fix: error importing config variables

--- api/src/server.js
+++ api/src/server.js
@@ -1,7 +1,7 @@
 'use strict'
 
 import api from './api.js'
-import SERVER_PORT from './config.js'
+import { SERVER_PORT } from './config.js'
 
 api.listen(SERVER_PORT)
 console.log('Running at port ' + SERVER_PORT)
