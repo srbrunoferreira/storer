cc2919659e2de13619671bd436fb6a08504f3c8a
Bruno Ferreira <srbrunoferreira@outlook.com>
2021-08-17 07:45:51 -0300

    add: new constants for databse connection

--- api/src/config.js
+++ api/src/config.js
@@ -1,5 +1,7 @@
 'use strict'
 
 const SERVER_PORT = 3000
+const DATABASE_USER = 'storer'
+const DATABASE_PASSWD = 'oUsqRocGs0uayi46'
 
-export default SERVER_PORT
+export { SERVER_PORT, DATABASE_USER, DATABASE_PASSWD }
