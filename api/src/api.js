fe7176f5b3e90d15a87a472a00cce508702fdeef
Bruno Ferreira <srbrunoferreira@outlook.com>
2021-08-22 14:05:49 -0300

    add: allow httpd delete method

--- api/src/api.js
+++ api/src/api.js
@@ -31,7 +31,7 @@ api.use(express.urlencoded({ extended: false, limit: '5mb' }))
 api.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*')
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-access-token')
-  res.header('Access-Control-Allow-Methods', 'GET, POST')
+  res.header('Access-Control-Allow-Methods', 'GET, POST, DELETE')
   next()
 })
 
